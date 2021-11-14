655893766e99a161146d1c31f46879b6b1fb3619
ali farhat <afarhat@hct.ac.ae>
2021-11-14 12:37:03 +0400

    Initial Commit

---
+++ MyShoppingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyShoppingList
+//
+//  Created by AFARHAT on 11/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
